3030c408c3d00bea0def84b2cb4eefe4d0b57235
anisur2805 <anisur2805@gmail.com>
2021-10-09 22:03:31 +0600

    test

--- home.php
+++ home.php
@@ -1,3 +1,3 @@
 <?php
 
-echo "Hello from HOme";
+echo date( "D" );
--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo "Hello world";
+$arr = [1,3,4,6,7,8,9];
+foreach ($arr as $key => $value) {
+    $value = $value * 2;
+}
+echo '<pre>';
+    print_r($value );
+echo '</pre>';
+ 
